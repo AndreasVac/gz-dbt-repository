5c5bc3c037e5027b7e438331cc8620bb0c6cb75f
Andreas <andreasvaccari93@gmail.com>
2024-12-17 16:44:08 +0000

    intermediate tasks

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        log_cost,
+        logCost as log_cost,
         cast(ship_cost as FLOAT64) as ship_cost
 
     from source
